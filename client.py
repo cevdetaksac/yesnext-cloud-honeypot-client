67bdf0db76479d5a631a2590219b0cb7433bc5f4
Local Dev <dev@local>
2025-09-07 16:13:03 +0300

    chore(release): bump version to 1.4.2

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ except Exception:
 
 # ===================== KURULUM & SABİTLER ===================== #
 TEST_MODE = 0  # 1=log only, 0=real
-__version__ = "1.4.1"
+__version__ = "1.4.2"
 
 GITHUB_OWNER = "cevdetaksac"
 GITHUB_REPO  = "yesnext-cloud-honeypot-client"
